7604db2df3089822d0c07023e0c15d82271cff2e
EDnotSheeran <juninho.tab3@gmail.com>
2019-11-18 16:55:16 -0300

    remove da past ong

--- ong/ongProjeto/wordpress/wp-content/themes/vantage/admin/about/page-free.php
+++
@@ -1,5 +0,0 @@
-<h3><?php _e( 'Forever Free', 'vantage' ) ?></h3>
-<p>
-	<?php _e( "Vantage is a completely free WordPress theme.", 'vantage' ) ?>
-	<?php _e( "We'll continue developing and enhancing it for years to come.", 'vantage' ) ?>
-</p>
